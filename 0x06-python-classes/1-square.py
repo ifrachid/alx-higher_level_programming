d0d81166f11e513c57fc351727c83926337553a8
rachid <rachid.iflou@gmail.com>
2024-01-24 08:03:44 -0800

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,10 +8,10 @@ class Square:
     Attributes:
         size: size of a square (1 side).
     """
-    def __init__(self, size):
-        """Creates new instances of square (1 side).
+def __init__(self, size):
+    """Creates new instances of square (1 side).
 
-        Args:
-            size: size of the square.
-        """
-        self.__size = size
+     Args:
+     size: size of the square.
+     """
+     self.__size = size
